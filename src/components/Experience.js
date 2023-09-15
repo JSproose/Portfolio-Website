81f89e06422b3c07d32e3a31ad1e958fe7d0bfb6
Jasper Calder <jaspe1@v1040-wn-rt-a-73-208.campus-dynamic.uwaterloo.ca>
2023-09-15 08:41:19 -0400

    Added SohoVFX experience

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,6 +1,7 @@
 import { Carousel, Container, Row, Col } from 'react-bootstrap';
 import maxonImg from '../assets/img/maxon-one-banner.png';
 import waterlooImg from '../assets/img/waterloo-floor-plan.pdf';
+import sohoImg from '../assets/img/sohoVFX.jpeg';
 
 
 export const Experience = () => {
@@ -12,6 +13,19 @@ export const Experience = () => {
                     <Col>
                         <h2>EXPERIENCE</h2>
                         <Carousel id="experience-carousel" interval={null}>
+                            <Carousel.Item>
+                                <div className='experience-item'>
+                                <h1>Software Developer</h1>
+                                <h3>Soho VFX | 05/2023 – 08/2023</h3>
+                                <img src={sohoImg}/>
+                                <ul>
+                                    <li>Collaborated with artists to create asset pipeline improvements using PyQt and the Houdini, Maya, and Nuke APIs.</li>
+                                    <li>Created a Maya plugin command using MEL, C++, and JSON to serialize and deserialize custom shader node networks for artists to easily copy, paste, and perform other operations on shader materials.</li>
+                                    <li>Built command-line tools using Python to automate the filtering, serializing into CSV, and packaging of Maya scenes, saving artists an estimated week of labour per project archived or asset delivered to vendor.</li>
+                                    <li>Designed a custom Maya and Houdini file save dialog using PyQt to automatically detect scene attributes and streamline file path naming.</li>
+                                </ul>
+                                </div>
+                            </Carousel.Item>
                             <Carousel.Item>
                                 <div className='experience-item'>
                                 <h1>Software Developer</h1>
